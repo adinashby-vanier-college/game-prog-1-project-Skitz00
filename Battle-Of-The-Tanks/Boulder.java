99803d0bed53f9695bf2eb6480e423205995f486
Vlaicu Suba <vlaicu.suba@gmail.com>
2024-11-10 16:37:37 -0500

    added a new boulder image and added a constructor to scale it because it was too big

--- Battle-Of-The-Tanks/Boulder.java
+++ Battle-Of-The-Tanks/Boulder.java
@@ -10,6 +10,16 @@ public class Boulder extends NPCs
 {
 
     /**
+     * 
+     */
+    public Boulder()
+    {
+        GreenfootImage boulder = getImage();
+        boulder.scale(100, 100);
+        setImage(boulder);
+    }
+
+    /**
      * Act - do whatever the Boulder wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
--- Battle-Of-The-Tanks/IceWorld.java
+++ Battle-Of-The-Tanks/IceWorld.java
@@ -24,27 +24,27 @@ public class IceWorld extends World
      */
     private void prepare()
     {
-        Player1 player1 =  new  Player1();
+        Player1 player1 =  new Player1();
         addObject(player1, 49, 379);
-        Player2 player2 =  new  Player2();
+        Player2 player2 =  new Player2();
         addObject(player2, 1158, 386);
-        Snowman snowman =  new  Snowman();
+        Snowman snowman =  new Snowman();
         addObject(snowman, 796, 186);
-        Snowman snowman2 =  new  Snowman();
+        Snowman snowman2 =  new Snowman();
         addObject(snowman2, 236, 604);
-        Snowman snowman3 =  new  Snowman();
+        Snowman snowman3 =  new Snowman();
         addObject(snowman3, 644, 441);
         snowman.setLocation(724, 174);
         snowman3.setLocation(775, 485);
-        Icicle icicle =  new  Icicle();
+        Icicle icicle =  new Icicle();
         addObject(icicle, 272, 183);
-        Icicle icicle2 =  new  Icicle();
+        Icicle icicle2 =  new Icicle();
         addObject(icicle2, 540, 320);
-        Icicle icicle3 =  new  Icicle();
+        Icicle icicle3 =  new Icicle();
         addObject(icicle3, 470, 535);
-        Icicle icicle4 =  new  Icicle();
+        Icicle icicle4 =  new Icicle();
         addObject(icicle4, 960, 652);
-        Icicle icicle5 =  new  Icicle();
+        Icicle icicle5 =  new Icicle();
         addObject(icicle5, 901, 277);
     }
 }
